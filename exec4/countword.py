01be9cad1b2489ecd0fb63315795fa2920d58a2f
liuyajune <liuyajune@126.com>
2017-03-25 14:52:39 +0800

    count line

--- exec4/countword.py
+++ exec4/countword.py
@@ -1,1 +1,21 @@
-import PIL
+#!usr/bin/env python
+#-*-  coding: utf-8 -*-
+
+import os
+
+def countWord(fileName=None):
+    lines =  0
+    try:
+        if fileName==None:
+            raise("error open file")
+        else:
+            with open(fileName ,'r') as f:
+                for i  in  f.readline():
+                    lines += 1
+    except  ZeroDivisionError as e:
+        print(e)
+    finally:
+        #print('finally')
+    return  lines
+
+print(countWord('test.txt'))
